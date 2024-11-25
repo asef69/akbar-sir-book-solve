5116de58b6274ff7cd7d8c16cacefd8799b980d7
asef69 <asefkabir97@gmail.com>
2024-11-25 18:25:32 +0600

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # akbar sir book solve
  Solve of important questions
+ Hello, This is Asef
+ Student ID:2305084
--- chapter 5/4.c
+++ chapter 5/4.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main(){
     int *A=(int *)malloc(5*sizeof(int));
     int *B=(int *)malloc(5*sizeof(int));
@@ -7,11 +8,12 @@ int main(){
         scanf("%d %d",(A+i),(B+i));
         i++;
     }
-    int C[10];
+    int *C=(int *)malloc(10*sizeof(int));
     int j=0;
     while(j<5){
        C[j]=*(A+j);
        C[j+5]=*(B+j);
+       j++;
     }
     for(int i=0;i<10;i++){
         printf("%d ",*(C+i));
